27c08e99b483176780cbf63bb57cf11ac80eb3ba
miyakawa2000 <m.ryohei.ist@gamil.com>
2022-06-05 22:52:11 +0900

    fix collision decision

--- main.py
+++ main.py
@@ -9,75 +9,77 @@ def RandomColor():
     return (R, G, B)
 
 def main():
-    # pygameの初期化
+    # init pygame
     pygame.init()
-    # メイン画面（Surface）初期化(横, 縦)
-    width = 300
-    height = 300
+    # init the display
+    width = 600
+    height = 600
     main_surface = pygame.display.set_mode((width, height))
-    # メイン画面のタイトル
-    pygame.display.set_caption("Pygame Sample")
-    # フォントオブジェクト生成（引数：フォント名とフォントサイズ）
-    # フォント名にNoneを指定するとPygameの既定のフォントになる
-    font = pygame.font.Font(None, 30)
-    # テキストのSurfaceオブジェクトの生成（引数：テキスト内容、antialias、文字の色RGB）
-    text_surface = font.render("DVD", True, (0, 0, 255))
-    # メイン画面の色設定（引数：RGB）
-    main_surface.fill((220, 220, 220))
-    # メイン画面上にテキストを配置（引数：配置するSurface、座標）
-    x = rd.randrange(0, width)
-    y = rd.randrange(0, height)
+    # set the title of main display
+    pygame.display.set_caption("DVD Taiki")
+    # generate the font object（value：(font type, font size)）
+    # if font type is "None", it's set as the default font of pygame
+    font = pygame.font.Font(None, 60)
+    # generate the text surface object（value：text, antialias: bool, (R, G, B)）
+    text_color = RandomColor()
+    text_surface = font.render("DVD", True, text_color)
+    # set the text width and height
+    text_width = text_surface.get_width()
+    text_height = text_surface.get_height()
+    # set the color of main display（value：(R,G,B)）
+    white = (220, 220, 220)
+    main_surface.fill(white)
+    # set the text on the main display（value：(surface, coordinate)）
+    x = rd.randrange(0, width - text_width)
+    y = rd.randrange(0, height - text_height)
     main_surface.blit(text_surface, (x, y))
-    # メイン画面の更新
+    # update the main display
     pygame.display.update()
-    # Clockオブジェクトの生成
+    # set the clock object
     clock = pygame.time.Clock()
-    # 速度の初期値
-    vx = 2
-    vy = -2
+    # set initial velocity
+    vx = 4
+    vy = -4
 
-    # ループを続けるかのフラグ
+    # flag for loop
     going = True
-    # 終了イベント発生までループをまわす
+    # main loop
     while going:
-        # イベントを取得
+        # get a event
         for event in pygame.event.get():
-            # 終了イベント（画面の×ボタン押下など）の場合、
-            # ループを抜ける
+            # if event type is quit, set the flag False
             if event.type == pygame.QUIT:
                 going = False
 
-        # 画面初期化
-        main_surface.fill((220, 220, 220))
-        # 移動
+        # init main surface
+        main_surface.fill(white)
+        # move
         x += vx
         y += vy
 
-        # 衝突判定
-        if x < 0 or x > width:
+        # collision decision
+        if x <= 0 or x + text_width >= width:
             vx *= -1
             vx += rd.uniform(-1, 1)
-            color = RandomColor()
-            text_surface = font.render("DVD", True, color)
-        if y < 0 or y > height:
+            text_color = RandomColor()
+            text_surface = font.render("DVD", True, text_color)
+        if y <= 0 or y + text_height >= height:
             vy *= -1
             vy += rd.uniform(-1, 1)
-            color = RandomColor()
-            text_surface = font.render("DVD", True, color)
+            text_color = RandomColor()
+            text_surface = font.render("DVD", True, text_color)
 
-        # 描画
+        # depict the text
         main_surface.blit(text_surface, (x, y))
-        # 更新
+        # update the display
         pygame.display.update()
 
-        # フレームレート（1秒間に何回画面を更新するか）の設定
+        # set the frame rate
         clock.tick(30)
 
-
-    # 終了処理
+    # quit
     pygame.quit()
     sys.exit()
-
 
 if __name__ == '__main__':
     main()
